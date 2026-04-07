53aa2f90a606731529ad0a5806074caba0bb0ef8
yls <3067579041@qq.com>
2026-04-07 21:27:39 +0800

    浏览历史

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import news, users, favorite
+from routers import news, users, favorite, history
 from fastapi.middleware.cors import CORSMiddleware
 
 from utils.exception_handlers import register_exception_handlers
@@ -25,3 +25,4 @@ async def root():
 app.include_router(news.routers)
 app.include_router(users.router)
 app.include_router(favorite.router)
+app.include_router(history.router)
